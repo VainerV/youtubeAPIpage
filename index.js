ecf1ae8d4f61bf48029cc267cd89b95941fa6d2b
VainerV <vainerv@yahoo.com>
2018-03-14 22:23:50 -0700

    Updated Code

---
+++ index.js
@@ -0,0 +1,15 @@
+// create responsive page that loads to all screen sizes.
+// Page contain Plage Logo, text field, submit (search) button,area that will
+//   show search results and video player.
+
+// Create JS form taht accepts request.
+//   form will contain lable, text field, submit button.
+
+// function that submits search request 
+// function that pulls search results.
+// function that show 6 images(thumbs) of the videos
+// function that activates each thumb-image 
+
+
+
+// function that clears the page. 
